e8f4f18540f48120d9fd13a2e58606e215ff2b9a
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-21 18:01:25 -0700

    Test backend connection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://blog-a2-server.up.railway.app/api/:path*',
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
