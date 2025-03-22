1a71b51159a2f828a8af056ab33c60d610e732d6
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-21 20:07:26 -0700

    try to fetch data from db again

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
 
 const Home = async () => {
+  const postResponse = await fetch('https://blog-a2.vercel.app/api', {
+    cache: 'no-store',
+  })
 
-const postResponse = await fetch('https://blog-a2.vercel.app/api', {
-  cache: 'no-store'
-})
-
-const post = await postResponse.json()
-
-console.log(post)
+  const post = await postResponse.json()
 
   return (
-    <div className='py-6'>
-      <h1 className='text-3xl font-bold tracking-tight mb-8'>Latest posts</h1>
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <h1>{post.toString()}</h1>
       </div>
     </div>
   )
