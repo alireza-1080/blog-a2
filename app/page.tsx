f6115fef103c674e374ec9eb460111032bfc68fb
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-21 20:08:51 -0700

    another try to fetch

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const Home = async () => {
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <h1>{post.toString()}</h1>
+        <h1>{post[0].content}</h1>
       </div>
     </div>
   )
