69c49e59c5e7c5c6b417b9c493fd265a129e07c8
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-25 07:35:19 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,54 @@
+// import React from 'react'
+
+// const Home = async () => {
+//   const postResponse = await fetch('https://blog-a2.vercel.app/api', {
+//     credentials: 'include',
+//     cache: 'no-store',
+//   })
+
+//   const post = await postResponse.json()
+
+//   console.log(post[0])
+
+//   return (
+//     <div className="py-6">
+//       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
+
+//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+//         <h1>{post[0].content}</h1>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Home
+'use client'
 import React from 'react'
 
-const Home = async () => {
-  const postResponse = await fetch('https://blog-a2.vercel.app/api', {
-    credentials: 'include',
-    cache: 'no-store',
-  })
+const Page = () => {
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://blog-a2.vercel.app/api', {
+          credentials: 'include',
+          cache: 'no-store',
+        })
 
-  const post = await postResponse.json()
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
 
-  console.log(post[0])
+        const data = await response.json()
+        console.log('Fetched data:', data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
 
-  return (
-    <div className="py-6">
-      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
+    fetchData()
+  }, [])
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <h1>{post[0].content}</h1>
-      </div>
-    </div>
-  )
+  return <div>page</div>
 }
 
-export default Home
+export default Page
