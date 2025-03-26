8061a76cbc8d3d486c4c9aa6dbfd4e83f45a2ac9
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-26 05:53:53 -0700

    3/26/2025 5:53 AM

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,14 @@
 'use client'
 import React from 'react'
-import axios from 'axios'
 
 const Home = () => {
-  const [data, setData] = React.useState<string>('')
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const postResponse = await axios.get('https://blog-a2.vercel.app/api', {
-        withCredentials: true,
-      })
-
-      const post = postResponse.data
-
-      setData(post[0].content)
-    }
-
-    fetchData()
-  }, [])
-
+    
   return (
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <h1>{data}</h1>
+        <h1>Posts will be here</h1>
       </div>
     </div>
   )
---
+++ components/logical/UpdateSession.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react'
+
+const UpdateSession = () => {
+  const updateSession = async () => {
+    try {
+      await fetch('https://blog-a2-server.up.railway.app/api', {
+        cache: 'no-store',
+        credentials: 'include',
+      })
+    } catch (error) {
+      console.error('Failed to update session:', error)
+    }
+  }
+
+  React.useEffect(() => {
+    updateSession()
+  }, [])
+
+  return <></>
+}
+
+export default UpdateSession
