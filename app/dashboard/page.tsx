f5f2bfea78c9c19595004680646ff750d99839b3
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-31 12:25:59 -0700

    3/31/2025 12:25 PM

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import isUserLoggedInFunc from '@/utils/isUserLoggedIn'
+import { redirect } from 'next/navigation'
 
-const Dashboard = () => {
+const Dashboard = async () => {
+
+  const isUserLoggedIn = await isUserLoggedInFunc()
+
+  if (!isUserLoggedIn) {
+   return redirect('/auth')
+  }
+
   return (
     <div>Dashboard</div>
   )
