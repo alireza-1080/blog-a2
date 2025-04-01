caf2e249c8a0414f91413dbf84ecd286a2198965
Alireza Aberoumand <maya7192@yahoo.com>
2025-03-31 22:01:17 -0700

    3/31/2025 10:01 PM

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,12 +5,12 @@ import React from 'react'
 const Dashboard = async () => {
   return (
     <div>
-        <div className='flex items-center justify-between mb-4'>
-            <h2 className='text-xl font-medium'>Your Blog Articles</h2>
-            <Link href={'/dashboard/create'}>
-              <Button className='cursor-pointer'>Create Post</Button>
-            </Link>
-        </div>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium">Your Blog Articles</h2>
+        <Link href={'/dashboard/create'}>
+          <Button className="cursor-pointer">Create Post</Button>
+        </Link>
+      </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="py-6">
+    <div>
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
