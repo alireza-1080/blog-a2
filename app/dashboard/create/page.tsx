9495bb71f3273dd69c40920c01271f4735dca66e
Alireza Aberoumand <maya7192@yahoo.com>
2025-04-01 03:20:54 -0700

    4/1/2025 3:20 AM

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -13,8 +13,8 @@ import toast from 'react-hot-toast'
 const CreateBlog = () => {
   const fileInputElement = React.useRef<HTMLInputElement | null>(null)
 
-  const [title, setTitle] = React.useState<string>("")
-  const [content, setContent] = React.useState<string>("")
+  const [title, setTitle] = React.useState<string>('')
+  const [content, setContent] = React.useState<string>('')
   const [image, setImage] = React.useState<File | null>(null)
   const [previewUrl, setPreviewUrl] = React.useState<string>('/png/image.png')
 
@@ -30,37 +30,51 @@ const CreateBlog = () => {
 
   const handleRemove = () => {
     setImage(null)
-    
+
     if (fileInputElement.current) {
       fileInputElement.current.value = ''
     }
   }
 
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    
-    const {success, data, error} = articleSchema.safeParse({title, content})
+
+    const { success, data, error } = articleSchema.safeParse({ title, content })
 
     if (!success) {
-        toast.error(error.errors[0].message)
-        return
+      toast.error(error.errors[0].message)
+      return
     }
 
-    
     if (!image) {
-        toast.error('Post image is required')
-        return
+      toast.error('Post image is required')
+      return
     }
-    
-    toast.success("PASSED")
-    console.log(data)
+
+    const formData = new FormData()
+    formData.append('image', image)
+    formData.append('title', data.title)
+    formData.append('content', data.content)
+    console.log(image)
+    try {
+      const response = await fetch('/api/article/create', {
+        method: 'POST',
+        credentials: 'include',
+        body: formData,
+      })
+
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   React.useEffect(() => {
     if (image) {
-        const imageUrl = URL.createObjectURL(image)
-        setPreviewUrl(imageUrl)
-        return
+      const imageUrl = URL.createObjectURL(image)
+      setPreviewUrl(imageUrl)
+      return
     }
 
     setPreviewUrl('/png/image.png')
@@ -77,22 +91,22 @@ const CreateBlog = () => {
           <form className="flex flex-col gap-4" onSubmit={handleFormSubmit}>
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Title</Label>
-              <Input id="title" value={title} onChange={e => setTitle(e.target.value)}/>
+              <Input id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
             </div>
             <div className="flex flex-col gap-2">
               <Label htmlFor="content">Content</Label>
-              <Textarea id="content" value={content} onChange={e => setContent(e.target.value)}></Textarea>
+              <Textarea id="content" value={content} onChange={(e) => setContent(e.target.value)}></Textarea>
             </div>
             <div className="flex flex-col justify-between items-center gap-4">
               <Label htmlFor="picture" className="self-start">
                 Image
               </Label>
               <div className="flex gap-2 order-2">
-                <Button type='button' className="w-fit" onClick={handleButtonClick}>
+                <Button type="button" className="w-fit" onClick={handleButtonClick}>
                   Select
                 </Button>
-                <Button type='button' className="w-fit" variant={'outline'} onClick={handleRemove}>
-                  <h3 className='text-red-500'>Remove</h3>
+                <Button type="button" className="w-fit" variant={'outline'} onClick={handleRemove}>
+                  <h3 className="text-red-500">Remove</h3>
                 </Button>
                 <Input
                   ref={fileInputElement}
@@ -105,12 +119,17 @@ const CreateBlog = () => {
               </div>
               <div className="w-2/3 order-1">
                 <AspectRatio ratio={16 / 9} className="bg-muted rounded-xl flex justify-center items-center">
-                {
-                    image ? 
-                    <Image src={image ? previewUrl : ""} alt="image-preview" fill className="rounded-xl"></Image>
-                    :
-                    <Image src={previewUrl} alt="image-preview" width={160} height={90} className="rounded-xl w-1/3"></Image>
-                }
+                  {image ? (
+                    <Image src={previewUrl} alt="image-preview" fill className="rounded-xl"></Image>
+                  ) : (
+                    <Image
+                      src={previewUrl}
+                      alt="image-preview"
+                      width={160}
+                      height={90}
+                      className="rounded-xl w-1/3"
+                    ></Image>
+                  )}
                 </AspectRatio>
               </div>
             </div>
