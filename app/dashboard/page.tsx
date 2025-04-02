8303ba6ac1f80848929b8af7259a3c6c8839eaaa
Alireza Aberoumand <maya7192@yahoo.com>
2025-04-02 04:28:40 -0700

    4/2/2025 4:28 AM

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,11 +3,35 @@ import { cookies } from 'next/headers'
 import Link from 'next/link'
 import React from 'react'
 
+type Author = {
+  username: string,
+  role: string,
+  image: string,
+  id: string
+}
+
+type Post = {
+  id: string
+  title: string
+  content: string
+  image: string
+  author: Author
+}
+
 const Dashboard = async () => {
   const cookieStore = await cookies()
   const auth_token = cookieStore.get('auth_token')?.value
-  console.log(auth_token)
-  
+  const postsResponse = await fetch('https://blog-a2.vercel.app/api/article/get-articles-by-user-id', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${auth_token}`,
+    },
+    credentials: 'include',
+  })
+
+  const postsData = await postsResponse.json()
+
+  const posts: Post[] | []= postsData.blogPosts
 
   return (
     <div>
@@ -17,6 +41,23 @@ const Dashboard = async () => {
           <Button className="cursor-pointer">Create Post</Button>
         </Link>
       </div>
+      {posts.length && posts.map((post, index) => {
+        return (<div key={post.id}>
+          <h2>{`Article #${index+1}`}</h2>
+          <h2>{post.title}</h2>
+          <h2>{post.content}</h2>
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+        </div>)
+      })}
     </div>
   )
 }
