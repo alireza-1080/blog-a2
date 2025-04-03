4bb9a204b103845b9055379f723e6c0a9c233fed
Alireza Aberoumand <maya7192@yahoo.com>
2025-04-03 05:01:39 -0700

    4/3/2025 5:01 AM

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -44,7 +44,7 @@ const Dashboard = async () => {
         </Link>
       </div>
 
-      {posts.length && (
+      {posts.length !== 0 && (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {posts.map((post) => {
             return <BlogPostCard post={post} key={post.id} />
